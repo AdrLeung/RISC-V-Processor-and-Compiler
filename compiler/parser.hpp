e477d636d95f7a2a62b7f061df361ed7ad2b7621
AdrLeung <adrian.leung4228@gmail.com>
2026-05-31 11:45:57 -0700

    added parser.hpp

---
+++ compiler/parser.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "lexer.hpp"
+#include "ast.hpp"
+#include <memory>
+
+class Parser {
+    private:
+        Lexer& lexer;
+        Token curr;
+
+        void advance();
+        void expect(Category c);
+
+        std::unique_ptr<ExpressionNode> parseExpression();
+        std::unique_ptr<StatementNode> parseStatement();
+        std::unique_ptr<FunctionNode> parseFunction();
+
+
+    public:
+        Parser(Lexer& l);
+        std::unique_ptr<ASTNode> parse();
+
+}
